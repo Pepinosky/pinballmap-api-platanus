a4175486f73c4188bb159f2021fda4f938e2215a
Pepinosky <aastorgaestevan4@gmail.com>
2021-11-10 22:24:45 -0300

    locations ready

--- pinballmap.rb
+++ pinballmap.rb
@@ -12,6 +12,8 @@ class PinballMap
     def locations(region_name)
         self.class.get("/region/#{region_name}/locations")
     end
+
+    
 end
 
 class Regions 
@@ -35,22 +37,53 @@ end
 class Locations
     def get_locations_from_region(region_names)
         locations= PinballMap.new
+        i=1
+        all_locations= Hash.new
         region_names.values.each do |region_name|         
-          location= locations.locations(region_name)["locations"]
-            location.each do |l|
-                pp l
-            end    
+          location= locations.locations(region_name)["locations"]       
+             location.each do |l|
+                all_locations["location_#{i}"]= l
+                i = i + 1
+             end    
         end
-
+        #locations_total = all_locations.legnth - 1
+        return all_locations
     end
+
+    def get_locations_number(locations)
+        return locations.length
+    end    
+ 
 end
+
+
+class Machines
+    def get_machine_total
+    end    
+end    
 
   region_name = Regions.new
   locations= Locations.new
 
-  pp region_name.get_first_n_regions_name(1)
-  locations.get_locations_from_region(region_name.get_first_n_regions_name(2)) 
- 
+  all_locations = locations.get_locations_from_region(region_name.get_first_n_regions_name(1))
+  locations_total = locations.get_locations_number(all_locations)
+  
+  pp all_locations
+  pp "cantidad de ubicaciones: #{locations_total}"
+
+
+  pp "promedio de maquinas: #"
+
+ # total_machine = 0
+#     regions.each do |region|
+#         locations= pinball_api.locations(region_name)
+#           location= locations["locations"]
+        
+#           location.each do |l|
+#               total_machine = total_machine + l["num_machines"]
+            
+#          end
+#      end
 
 
 
