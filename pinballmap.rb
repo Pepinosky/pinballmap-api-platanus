e825acb1a8f570552d585122881e686ca732d01b
Pepinosky <aastorgaestevan4@gmail.com>
2021-11-11 22:45:56 -0300

    location with more machine statistic added, todo: location with less machine

--- pinballmap.rb
+++ pinballmap.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'pp'
+require 'hash_dig_and_collect'
 class PinballMap
     include HTTParty
     base_uri 'https://pinballmap.com/api/v1'
@@ -8,13 +9,19 @@ class PinballMap
     def regions
         self.class.get('/regions')
     end
-
+    def location_and_machine_counts(region_name)
+        parameters = {query: {region_name: region_name}}
+        self.class.get("/regions/location_and_machine_counts", parameters)
+    end
     def locations(region_name)
         self.class.get("/region/#{region_name}/locations")
     end
 
+   
+
     
 end
+
 
 class Regions 
     def get_first_n_regions_name(n)
@@ -52,39 +59,97 @@ class Locations
 
     def get_locations_number(locations)
         return locations.length
-    end    
- 
+    end
+    
+    def get_locations_machines(region_names)
+        locations_machines = PinballMap.new
+        total_machines = 0
+        region_names.values.each do |region_name|         
+              total_machines += locations_machines.location_and_machine_counts(region_name)["num_lmxes"]
+        end
+        return total_machines
+    end
+
+    def get_locations_with_more_machines(region_names)
+        locations= PinballMap.new
+        location_with_more_machines_count= 0
+        location_with_more_machines_name= nil
+        count= 0
+        
+        region_names.values.each do |region_name|         
+            location= locations.locations(region_name)["locations"]       
+            location.each do |l|
+              if l["num_machines"] >= location_with_more_machines_count 
+                location_with_more_machines_count= l["num_machines"]
+                location_with_more_machines_name = l["name"]
+              end 
+            end  
+        end
+        return location_with_more_machines_name
+          
+    end
+    
+    def get_locations_with_less_machines(region_names)
+        locations= PinballMap.new
+        location_with_less_machines_count= 0
+        location_with_less_machines_name= nil
+        
+        
+        region_names.values.each do |region_name|         
+            location= locations.locations(region_name)["locations"]       
+            location.each do |l|
+              if l["num_machines"] <= location_with_less_machines_count 
+                location_with_less_machines_count= l["num_machines"]
+                location_with_less_machines_name = l["name"]
+              end 
+            end  
+        end
+        return location_with_less_machines_count
+          
+    end  
+    
+
 end
 
 
-class Machines
-    def get_machine_total
-    end    
-end    
-
-  region_name = Regions.new
-  locations= Locations.new
-
-  all_locations = locations.get_locations_from_region(region_name.get_first_n_regions_name(1))
-  locations_total = locations.get_locations_number(all_locations)
-  
-  pp all_locations
-  pp "cantidad de ubicaciones: #{locations_total}"
-
-
-  pp "promedio de maquinas: #"
-
- # total_machine = 0
-#     regions.each do |region|
-#         locations= pinball_api.locations(region_name)
-#           location= locations["locations"]
+# class Machines
+#     def get_machine_total_all_locations(all_locations)
+#          total_machine = 0
+#            location= locations["locations"]
         
 #           location.each do |l|
-#               total_machine = total_machine + l["num_machines"]
+#                total_machine = total_machine + l["num_machines"]
             
-#          end
-#      end
+#           end
+      
+#     end    
+# end    
 
+  region = Regions.new
+  locations= Locations.new
+  #count= PinballMap.new
+
+  
+  #conseguir las primeras n region names para poder obtener todas las location(ubicaciones) que tiene 
+  region_names = region.get_first_n_regions_name(1)
+  all_locations = locations.get_locations_from_region(region_names)
+  total_machines= locations.get_locations_machines(region_names)
+  locations_total = locations.get_locations_number(all_locations)
+  average_machines = total_machines/locations_total
+  #locations.get_locations_with_more_machines(all_locations)  
+  #locations_with_more_machines= 
+  location_with_more_machines = locations.get_locations_with_more_machines(region_names)
+  location_with_less_machines = locations.get_locations_with_less_machines(region_names)
+ #pp locations_with_more_machines 
+  
+   
+   pp "total de locations: #{locations_total}" 
+   pp "cantidad de maquinas: #{total_machines}"
+   pp "promedio de maquinas: #{average_machines.to_f}"
+   pp "ubicacion con mayor cantidad de maquinas: #{location_with_more_machines}"
+   pp "ubicacion con menor cantidad de maquinas: #{location_with_less_machines}"
+
+ 
 
 
   
